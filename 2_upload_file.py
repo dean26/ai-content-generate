40c033f229da66a5d9900c16cc419e7018176581
Lukasz Przezdziek <l.przezdziek@gmail.com>
2023-09-15 09:49:38 +0200

    Add upload file program

---
+++ 2_upload_file.py
@@ -0,0 +1,14 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+response = openai.File.create(
+  file=open("result.jsonl", "rb"),
+  purpose='fine-tune'
+)
+
+print(response)
